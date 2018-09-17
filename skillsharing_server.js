458a66cee2a4ca9447f2d268c2a094e6d7e7f8aa
allinthemind66 <r.nilon92@gmail.com>
2018-09-17 13:53:05 -0400

    add GET and DELETE routes to router

--- skillsharing_server.js
+++ skillsharing_server.js
@@ -7,6 +7,7 @@ const defaultHeaders = {"Content-Type": "text/plain"};
 
 class SkillShareServer {
   constructor(talks){
+    //talks is an object that stores all of the talks that have been created
     this.talks = talks;
     this.version = 0;
     this.waiting = [];
@@ -15,18 +16,25 @@ class SkillShareServer {
   this.server = createServer((request, response) => {
     let resolved = router.resolve(this, request);
     if(resolved) {
+      //if request matches one in our router...
       resolved.catch(error => {
         if (error.status != null) return error;
         return {body: String(error), status: 500};
       }).then(({body, status = 200, headers = defaultHeaders}) => {
         response.writeHead(status, headers);
+        //if there is data in response.end(data), it is equivalent of calling response.write(data) and
+        //then response.end()
         response.end(body)
       });
     } else{
+      //else get it from public directory
       fileServer(request, response);
     }
   });
+
+
  }
+
  start(port) {
    this.server.listen(port);
  }
@@ -34,3 +42,21 @@ class SkillShareServer {
    this.server.close();
  }
 }
+
+const talkPath = /^\/talks\/([^\/]+)$/;
+
+router.add("GET", talkPath, async (server, title) => {
+  if(title in server.talks) {
+    return {body: JSON.stringify(server.talks[title]), headers: {"Content-Type": "application/json"}};
+  } else {
+    return {status: 404, body: `No talk ${title} found`}
+  }
+})
+
+router.add("DELETE", talkPath, async (server, title) => {
+  if (title in server.talks) {
+    delete server.talks[title];
+    server.updated()
+  }
+  return {status: 204}
+})
